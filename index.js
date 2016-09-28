9a83e9da0d3f338e9916beb211d6692e9a35e021
Christian Talmo <talmo.christian@gmail.com>
2016-09-28 20:09:11 +0300

    basic css error detection

--- index.js
+++ index.js
@@ -34,3 +34,57 @@ var config = {
 }
 
 console.log(config)
+
+watch(config.target)
+exec(config.cmd) // TODO
+
+function watch (target) {
+  chokidar.watch(target).on('change', function () {
+    console.log('change on target [' + target + ']')
+  })
+}
+
+function exec (cmd) {
+  if (typeof cmd === 'string') cmd = cmd.split(' ')
+  var child = cp.spawn(cmd[0], cmd.slice(1))
+  console.log('exec cmd [' + cmd + ']')
+
+  // TODO check for error, send error log to client through sockets
+  var buffer = ''
+  var timeout = undefined
+  function process (chunk) {
+    var str = chunk.toString('utf8')
+    buffer += str
+
+    var split = buffer.split('\n')
+
+    var isError = false
+
+    var result = []
+    split.forEach(function (line) {
+      line = line.toLowerCase()
+      if (line.indexOf('error') !== -1) isError = true
+      if (line.indexOf('node_modules') === -1) {
+        result.push(line)
+      }
+    })
+
+    clearTimeout(timeout)
+    timeout = setTimeout(function () {
+      console.log(result.join('\n'))
+      buffer = ''
+
+      if (isError) {
+        console.log('----')
+        console.log('- error -')
+      }
+    }, 100)
+  }
+
+  child.stdout.on('data', process)
+  child.stderr.on('data', process)
+
+  child.on('close', function (code) {
+    console.log('SPAWN CLOSED')
+  })
+}
