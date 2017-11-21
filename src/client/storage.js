e063472d73fbf773ff2cc7cb8ae78725646a60c4
Christian Talmo <talmo.christian@gmail.com>
2017-11-21 20:55:52 +0200

    added storage set and get logic

--- src/client/storage.js
+++ src/client/storage.js
@@ -4,16 +4,24 @@ var api = {}
 
 api.get = function ( key ) {
   if ( window && window.localStorage ) {
-    window.localStorage.getItem( key )
+    var data = window.localStorage.getItem( key )
+    try {
+      return JSON.parse( data )
+    } catch ( err ) { /* ignore */ }
+    return data
   }
   return undefined
 }
 
 api.set = function ( key, data ) {
   if ( window && window.localStorage ) {
+    if ( typeof data === 'object' ) {
+      data = JSON.stringify( data )
+    }
     window.localStorage.setItem( key, data )
   }
   return undefined
 }
+api.save = api.set
 
 module.exports = api
