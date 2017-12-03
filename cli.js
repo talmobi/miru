8035059d25808c735b2ab68eebc8e8ad18d71359
Christian Talmo <talmo.christian@gmail.com>
2017-12-04 00:46:09 +0200

    refactored relative paths and bundling

--- cli.js
+++ cli.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 
+var fs = require( 'fs' )
 var path = require( 'path' )
-require( path.join( __dirname, 'dist', 'miru.js' ) )
+
+var assets = {}
+
+assets.packageJson = fs.readFileSync(
+  path.join( __dirname, 'package.json' )
+)
+
+assets.usage = fs.readFileSync(
+  path.join( __dirname, 'dist/usage.txt' )
+)
+
+assets.miruConnectSource = path.join( __dirname, 'dist/miru-connect.js' )
+
+assets.favicon = path.join( __dirname, 'dist/favicon.png' )
+
+assets.pesticide = path.join( __dirname, 'dist/pesticide.css' )
+
+assets.indexHTML = path.join( __dirname, 'dist/index.html' )
+
+require( path.join( __dirname, 'dist/miru.js' ) )( assets )
+
