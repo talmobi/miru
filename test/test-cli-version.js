88403df8bc687c89218a3dc733922ed5e52aee99
Christian Talmo <talmo.christian@gmail.com>
2018-09-02 02:00:09 +0300

    test added cli version

---
+++ test/test-cli-version.js
@@ -0,0 +1,81 @@
+const test = require( 'tape' )
+
+const cp = require( 'child_process' )
+
+var path = require( 'path' )
+
+var miruPath = path.join( __dirname, '..', 'srccli.js' )
+var publicPath = path.join( __dirname, 'test/stage' )
+
+const pkgj = require( '../package.json' )
+
+let spawns = []
+
+test( 'miru --version', function ( t ) {
+  t.timeoutAfter( 3000 )
+
+  const spawn = cp.spawn(
+    miruPath,
+    `
+    --version
+    `.trim().split( /\s+/ )
+  )
+  spawns.push( spawn )
+
+  spawn.stdout.on( 'data', out )
+  spawn.stderr.on( 'data', out )
+
+  let log = ''
+  function out ( chunk ) {
+    let msg = chunk.toString( 'utf8' )
+    log += msg
+  }
+
+  spawn.on( 'exit', function () {
+    t.equal(
+      log.trim(),
+      'miru version: ' + pkgj.version,
+      'cli --version OK!'
+    )
+
+    t.end()
+  } )
+} )
+
+test( 'miru -V', function ( t ) {
+  t.timeoutAfter( 3000 )
+
+  const spawn = cp.spawn(
+    miruPath,
+    `
+    -V
+    `.trim().split( /\s+/ )
+  )
+  spawns.push( spawn )
+
+  spawn.stdout.on( 'data', out )
+  spawn.stderr.on( 'data', out )
+
+  let log = ''
+  function out ( chunk ) {
+    let msg = chunk.toString( 'utf8' )
+    log += msg
+  }
+
+  spawn.on( 'exit', function () {
+    t.equal(
+      log.trim(),
+      'miru version: ' + pkgj.version,
+      'cli -V OK!'
+    )
+
+    t.end()
+  } )
+} )
+
+process.on( 'exit', function () {
+  try {
+    spawn && spawn.kill()
+  } catch ( err ) { /* ignore*/ }
+} )
+
