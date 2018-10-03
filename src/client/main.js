ed1a2ded89e3719c3802bbc4be3ead5430339606
Christian Talmo <talmo.christian@gmail.com>
2018-10-03 20:05:59 +0300

    added basic scroll y position retention between reloads

--- src/client/main.js
+++ src/client/main.js
@@ -6,6 +6,8 @@ import './dom-error-watcher.js'
 import './dom-console-watcher.js'
 import './net.js'
 
+import storage from './storage.js' // save scroll position
+
 // attach better monospace font if possible
 // the error modal.js uses Monaco ( OSX terminal font ) if available
 // otherwise 'Space Mono' or monospace
@@ -41,3 +43,18 @@ window.addEventListener( 'keyup', function ( evt ) {
 
 // TODO keep scroll position
 // TODO
+setTimeout( function () {
+  var y = storage.get( '__miru-scroll-y' ) || 0
+
+  if ( y ) {
+    console.log( ' == MIRU == y scroll position reset: ' + y )
+    window.scroll( 0, Number( y ) )
+  }
+
+  // remember scroll position before reloading
+  window.addEventListener( 'beforeunload', function () {
+    console.log( ' == MIRU == beforeunload' )
+    var y = window.scrollY
+    storage.set( '__miru-scroll-y', y )
+  } )
+}, 100 )
