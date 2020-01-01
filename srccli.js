ef1ef16717c5bd3c91bb62a19bae48f19ef68a43
Christian Talmo <talmo.christian@gmail.com>
2020-01-01 07:31:54 +0200

    refactor: moved asset loading to separate assets.js file

--- srccli.js
+++ srccli.js
@@ -1,25 +1,4 @@
 #!/usr/bin/env node
 
-var fs = require( 'fs' )
 var path = require( 'path' )
-
-var assets = {}
-
-assets.packageJson = fs.readFileSync(
-  path.join( __dirname, 'package.json' ), 'utf8'
-)
-
-assets.usage = fs.readFileSync(
-  path.join( __dirname, 'dist/usage.txt' ), 'utf8'
-)
-
-assets.miruConnectSource = path.join( __dirname, 'dist/miru-connect.js' )
-
-assets.favicon = path.join( __dirname, 'dist/favicon.png' )
-
-assets.pesticide = path.join( __dirname, 'dist/pesticide.css' )
-
-assets.indexHTML = path.join( __dirname, 'dist/index.html' )
-
-require( path.join( __dirname, 'src/server/miru.js' ) )( assets )
-
+require( path.join( __dirname, 'src/server/miru.js' ) )
