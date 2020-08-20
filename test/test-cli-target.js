056f0a0a5bc22733a3e6a2b4ac7cfc6dd131fbdb
Christian Talmo <talmo.christian@gmail.com>
2020-08-20 17:29:28 +0300

    test: add single file html target test

--- test/test-cli-target.js
+++ test/test-cli-target.js
@@ -137,3 +137,81 @@ test( 'test -t,--target', function ( t ) {
     } )
   } )
 } )
+
+test( 'test single -t,--target', function ( t ) {
+  t.timeoutAfter( 1000 * 25 )
+  t.plan( 2 )
+
+  prepareStage( function ( err ) {
+    t.error( err, 'stage prepared without errors' )
+
+    const spawn = cp.spawn(
+      miruPath,
+      `
+      -P ${ 40666 }
+      -p ${ publicPath }
+      -t ${ path.join( publicPath, 'index.html' ) }
+      `.trim().split( /\s+/ )
+    )
+    spawns.push( spawn )
+
+    spawn.stdout.on( 'data', out )
+    spawn.stderr.on( 'data', out )
+
+    let log = ''
+    function out ( chunk ) {
+      let msg = chunk.toString( 'utf8' )
+      log += msg
+
+      console.log( msg )
+
+      // we rely on this happening only once at the start
+      // when miru is setup and ready
+      if ( msg.indexOf( 'server listening' ) >= 0 ) {
+        setTimeout( function () {
+          cpyHTML(
+            function () {
+              end()
+            }
+          )
+        }, 1000 * 1 )
+      }
+
+      if ( _stdoutTriggerCallback ) {
+        var fn = _stdoutTriggerCallback
+        _stdoutTriggerCallback = undefined
+
+        setTimeout( function () {
+          fn()
+        }, 1000 * 2 ) // TODO unsure of this timeout, dynamically set through arg?
+      }
+    }
+
+    let _stdoutTriggerCallback
+    function _triggerOnStdout ( callback ) {
+      _stdoutTriggerCallback = callback
+    }
+
+    function cpyHTML ( done ) {
+      // rewrite file to trigger file watcher and target-build event
+      var text = fs.readFileSync( path.join( __dirname, 'stage', 'index.html' ), 'utf8' )
+      fs.writeFileSync( path.join( __dirname, 'stage', 'index.html' ), text, 'utf8' )
+
+      _triggerOnStdout( done )
+    }
+
+    function end () {
+      t.ok(
+        stripAnsi( log ).indexOf( '(file change detected) \nbroadcasting: test/stage/index.html' ) >= 0,
+        'index.html target-build OK!'
+      )
+
+      kill( spawn.pid )
+    }
+
+    spawn.on( 'exit', function () {
+      console.log( 'spawn exited.' )
+      t.end()
+    } )
+  } )
+} )
